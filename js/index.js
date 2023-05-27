79415d52e6cb2db4eccae1a61bf36c7c02158960
Shippu Kumar <117848490+shibu931@users.noreply.github.com>
2023-05-27 22:14:43 +0530

    index.js

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ let inputDir = {
     x: 0,
     y: 0
 };
-const foodSound = new Audio('/music/food.mp3');
-const gameOversound = new Audio('/music/gameover.mp3')
-const moveSound = new Audio("/music/move.mp3")
-const musicSound = new Audio('/music/music.mp3')
+const foodSound = new Audio('music/food.mp3');
+const gameOversound = new Audio('music/gameover.mp3')
+const moveSound = new Audio("music/move.mp3")
+const musicSound = new Audio('music/music.mp3')
 let speed = 5;
 let lastPaintTime = 0;
 let snakeArr = [
